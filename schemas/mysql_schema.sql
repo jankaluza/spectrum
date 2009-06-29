6c2a8824558adb8aa0baaaf2be9156ee11ee50c4
Jan Kaluza <hanzz@soc.pidgin.im>
2009-06-29 14:22:31 +0000

    Added vcards table schema

--- schemas/mysql_schema.sql
+++ schemas/mysql_schema.sql
@@ -30,3 +30,10 @@ CREATE TABLE `settings` (
   `value` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `vcards` (
+  `username` varchar(250) NOT NULL,
+  `vcard` text NOT NULL,
+  `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
