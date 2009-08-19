bcaf4dcf1e9435353b877c499c8dd77e1118a2ba
Jan "HanzZ" Kaluza <hanzz.k@gmail.com>
2009-08-19 09:13:34 +0200

    More debug on sql connect

--- sql.cpp
+++ sql.cpp
@@ -34,7 +34,7 @@ SQLClass::SQLClass(GlooxMessageHandler *parent){
 		dbi_conn_set_option(m_conn, "dbname", g_path_get_basename(p->configuration().sqlDb.c_str()));
 	} else {
 		dbi_conn_set_option(m_conn, "host", p->configuration().sqlHost.c_str());
-		dbi_conn_set_option(m_conn, "username", p->configuration().sqlUser.c_str());
+		dbi_conn_set_option(m_conn, "username", "test");
 		dbi_conn_set_option(m_conn, "password", p->configuration().sqlPassword.c_str());
 		dbi_conn_set_option(m_conn, "dbname", p->configuration().sqlDb.c_str());
 		dbi_conn_set_option(m_conn, "encoding", "UTF-8");
@@ -42,6 +42,10 @@ SQLClass::SQLClass(GlooxMessageHandler *parent){
 
 	if (dbi_conn_connect(m_conn) < 0) {
 		std::cout << "SQL CONNECTION FAILED\n";
+		const char *errmsg;
+		dbi_conn_error(m_conn, &errmsg);
+		if (errmsg)
+			Log().Get("SQL ERROR") << errmsg;
 	}
 	else {
 		initDb();
