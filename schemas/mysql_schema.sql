20b05fdb5889f64bb197862c905775609bb3b546
Jan "HanzZ" Kaluza <hanzz.k@gmail.com>
2009-08-19 11:55:59 +0200

    Update mysql_schema.sql.

--- schemas/mysql_schema.sql
+++ schemas/mysql_schema.sql
@@ -1,45 +1,32 @@
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `buddies` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `user_id` int(10) unsigned NOT NULL,
+  `uin` varchar(255) collate utf8_bin NOT NULL,
+  `subscription` enum('to','from','both','ask','none') collate utf8_bin NOT NULL,
+  `nickname` varchar(255) collate utf8_bin NOT NULL,
+  `groups` varchar(255) collate utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`,`uin`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+ 
+CREATE TABLE IF NOT EXISTS `buddies_settings` (
+  `buddy_id` int(10) unsigned NOT NULL,
+  `var` varchar(50) collate utf8_bin NOT NULL,
+  `type` smallint(4) unsigned NOT NULL,
+  `value` varchar(255) collate utf8_bin NOT NULL,
+  PRIMARY KEY (`buddy_id`,`var`),
+  KEY `buddy_id` (`buddy_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+ 
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `jid` varchar(255) collate utf8_bin NOT NULL,
-  `uin` varchar(255) collate utf8_bin NOT NULL,
+  `uin` varchar(4095) collate utf8_bin NOT NULL,
   `password` varchar(255) collate utf8_bin NOT NULL,
-  `language` varchar(5) collate utf8_bin NOT NULL,
-  `group` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`id`),
-  KEY `jid` (`jid`),
-  KEY `uin` (`uin`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-CREATE TABLE `rosters` (
-  `id` bigint(20) unsigned NOT NULL auto_increment,
-  `jid` varchar(100) collate utf8_bin NOT NULL,
-  `uin` varchar(100) collate utf8_bin NOT NULL,
-  `subscription` varchar(10) collate utf8_bin NOT NULL,
-  `nickname` varchar(255) collate utf8_bin NOT NULL,
-  `g` varchar(255) collate utf8_bin NOT NULL,
-  PRIMARY KEY  (`id`),
-  Unique Key `JidUin` (`jid`, `uin`),
-  KEY `jid` (`jid`),
-  KEY `uin` (`uin`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-CREATE TABLE `settings` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `jid` varchar(255) NOT NULL,
-  `var` varchar(255) NOT NULL,
-  `type` smallint(4) unsigned NOT NULL,
-  `value` varchar(255) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-CREATE TABLE `vips` (
-  `jid` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`jid`)
-) ENGINE=MYISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-CREATE TABLE IF NOT EXISTS `vcards` (
-  `username` varchar(250) NOT NULL,
-  `vcard` text NOT NULL,
-  `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`username`)
+  `language` varchar(25) collate utf8_bin NOT NULL,
+  `encoding` varchar(50) collate utf8_bin NOT NULL,
+  `last_login` datetime NOT NULL,
+  `vip` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `jid` (`jid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
