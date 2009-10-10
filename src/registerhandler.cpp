b0b13dea1a8d5505a388989548a7d7e1f3231fc6
Jan "HanzZ" Kaluza <hanzz.k@gmail.com>
2009-10-10 13:22:29 +0200

    Fixed registering with only_for_vip

--- src/registerhandler.cpp
+++ src/registerhandler.cpp
@@ -40,13 +40,6 @@ GlooxRegisterHandler::~GlooxRegisterHandler(){
 bool GlooxRegisterHandler::handleIq (const IQ &iq){
 	std::cout << "*** "<< iq.from().full() << ": iq:register received (" << iq.subtype() << ")\n";
 	User *user = p->userManager()->getUserByJID(iq.from().bare());
-	if (p->configuration().onlyForVIP){
-		bool isVIP = p->sql()->isVIP(iq.from().bare());
-		std::list<std::string> const &x = p->configuration().allowedServers;
-		if (!isVIP && std::find(x.begin(), x.end(), iq.from().server()) == x.end())
-			return false;
-	}
-
 
 	// send registration form
 	if(iq.subtype() == IQ::Get) {
