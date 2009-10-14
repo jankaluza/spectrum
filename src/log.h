129bb9054f3ec8dbe9772ff81567c7b76fef6725
Jan "HanzZ" Kaluza <hanzz.k@gmail.com>
2009-10-14 21:38:51 +0200

    Added date to Spectrum Log class

--- src/log.h
+++ src/log.h
@@ -17,8 +17,8 @@ public:
 
 	std::ostringstream& Get(const std::string &user) {
 		time_t now = time(0);
-		char timestamp_buf[10] = "";
-		strftime (timestamp_buf, 10, "%H:%M:%S", localtime(&now));
+		char timestamp_buf[25] = "";
+		strftime (timestamp_buf, 25, "%x %H:%M:%S", localtime(&now));
 		std::string timestamp = std::string(timestamp_buf);
 		os << "[" << timestamp << "] <" << user << "> ";
 		return os;
