51d9b577c532a8bf3e7979352c6c8bbc7160f9c9
Jan "HanzZ" Kaluza <hanzz.k@gmail.com>
2009-12-15 18:31:54 +0100

    Fix for crash when buddy->node.ui_data was set twice

--- src/user.cpp
+++ src/user.cpp
@@ -166,6 +166,7 @@ void User::purpleBuddyRemoved(PurpleBuddy *buddy) {
 void User::purpleBuddyCreated(PurpleBuddy *buddy) {
 	if (buddy==NULL || m_loadingBuddiesFromDB)
 		return;
+	buddy->node.ui_data = (void *) new SpectrumBuddy(-1, buddy);
 	SpectrumBuddy *s_buddy = (SpectrumBuddy *) buddy->node.ui_data;
 
 	handleBuddyCreated(s_buddy);
