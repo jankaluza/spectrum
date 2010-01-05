cb823c04d095002f2c76b3f5f75ada829eea1452
Mati <mati@lt.(none)>
2010-01-05 19:51:00 +0800

    added licence to all spectrumctl source files, spectrumctl now uses /usr/bin/python, because Ubuntu jaunty doesn't have a python2 binary

--- spectrumctl/spectrumctl.py
+++ spectrumctl/spectrumctl.py
@@ -1,4 +1,25 @@
-#!/usr/bin/env python2
+#!/usr/bin/python -Wignore::DeprecationWarning
+# -*- coding: utf-8 -*-
+#
+# spectrumctl can be used to control your spectrum-instances. Valid actions are
+# start, stop, restart and reload. By default, spectrumctl acts on all instances
+# defined in /etc/spectrum/
+#
+# Copyright (C) 2009, 2010 Mathias Ertl
+#
+# This file is part of Spectrumctl.
+#
+# Spectrumctl is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
 
 import os, sys
 from spectrum import *
