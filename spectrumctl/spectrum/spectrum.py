e371608893283bd2a222da3bb29ac9dda22a31a5
Mati <mati@lt.(none)>
2010-01-06 20:21:05 +0100

    spectrumctl now has --quiet and --su which starts spectrum as a different user.

--- spectrumctl/spectrum/spectrum.py
+++ spectrumctl/spectrum/spectrum.py
@@ -20,6 +20,9 @@ class spectrum():
 		except:
 			return -1
 
+	def su_cmd( self, cmd ):
+		return [ 'su', 'spectrum', '-s', '/bin/bash', '-c', ' '.join( cmd ) ]
+
 	def status( self ):
 		"""
 		Determines if the process is running.
@@ -54,13 +57,14 @@ class spectrum():
 				return 4, "unknown."
 
 	def start( self ):
-		status = self.status()
+		status = self.status()[0]
 		if status == 0:
 			return 0, "already running." # starting while we are already running is also success!
 		elif status != 3:
 			return 1, "status unknown." # We cannot start if status != 3
 
 		cmd = [ 'spectrum', '-c', self.config ]
+		cmd = self.su_cmd( cmd )
 		retVal = subprocess.call( cmd )
 		if retVal != 0:
 			return 1, "could not start spectrum instance."
@@ -68,7 +72,7 @@ class spectrum():
 		return 0, "ok."
 
 	def stop( self ):
-		status = self.status()
+		status = self.status()[0]
 		if status == 3:
 			# stopping while not running is also success!
 			return 0, "already stopped."
@@ -84,13 +88,13 @@ class spectrum():
 			return 1, "failed."
 
 	def restart( self ):
-		if self.stop() == 0:
+		if self.stop()[0] == 0:
 			return self.start()
 		else:
 			return 1, "could not stop spectrum instance."
 
 	def reload( self ): # send SIGHUP to process
-		if self.status() != 0:
+		if self.status()[0] != 0:
 			return 1, "not running."
 		pid = self.get_pid()
 		try:
--- spectrumctl/spectrumctl.py
+++ spectrumctl/spectrumctl.py
@@ -1,4 +1,25 @@
-#!/usr/bin/env python2
+#!/usr/bin/python -Wignore::DeprecationWarning
+# -*- coding: utf-8 -*-
+#
+# spectrumctl can be used to control your spectrum-instances. Valid actions are
+# start, stop, restart and reload. By default, spectrumctl acts on all instances
+# defined in /etc/spectrum/
+#
+# Copyright (C) 2009, 2010 Mathias Ertl
+#
+# This file is part of Spectrumctl.
+#
+# Spectrumctl is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
 
 import os, sys
 from spectrum import *
@@ -13,6 +34,10 @@ parser.add_option( '-c', '--config', metavar='FILE',
 	help = 'Only act on transport configured in FILE' )
 parser.add_option( '-d', '--config-dir', metavar='DIR', default='/etc/spectrum',
 	help = 'Act on all transports configured in DIR (default: %default)' )
+parser.add_option( '-q', '--quiet', action='store_true', default=False,
+	help = 'Do not print any output.' )
+parser.add_option( '--su', default='spectrum',
+	help = 'Start spectrum as this user (default: %default)' )
 options, args = parser.parse_args()
 action = args[0]
 
@@ -22,15 +47,18 @@ elif action not in dir( spectrum.spectrum ):
 	print( "Unknown action." )
 
 def act( instance ):
-	print( "%s %s..."%(action.title(), instance.get_jid()) ),
+	if not options.quiet:
+		print( "%s %s..."%(action.title(), instance.get_jid()) ),
 	# print( "%s %s..."%(action.title(), instance.get_jid()), end='' ) #python3
 	exit, string = getattr( instance, action )()
-	print( string )
+	if not options.quiet:
+		print( string )
 	return exit
 
 if options.config:
 	instance = spectrum.spectrum( options.config )
 	ret = act( instance )
+	print ret
 	sys.exit( ret )
 else:
 	if not os.path.exists( options.config_dir ):
