51888ce4c3aa7297360cf87daf2bd731e1010cf1
Jan "HanzZ" Kaluza <hanzz.k@gmail.com>
2010-01-22 20:19:43 +0100

    more debug for prepared statements

--- src/sql.cpp
+++ src/sql.cpp
@@ -114,7 +114,7 @@ SQLClass::~SQLClass() {
 void SQLClass::createStatements() {
 	if (!m_version_stmt) {
 		Log("NEW STATEMENT", "version_stmt");
-		m_version_stmt = new Statement( ( STATEMENT("SELECT ver FROM " + p->configuration().sqlPrefix + "db_version"), into(m_version) ) );
+		m_version_stmt = new Statement( ( STATEMENT("SELECT ver FROM " + p->configuration().sqlPrefix + "db_version") ) );
 	}
 	
 	// Prepared statements
