4699912ddb6fbbffd6240b843f5f026b509c08f6
Jan "HanzZ" Kaluza <hanzz.k@gmail.com>
2010-03-26 13:52:36 +0100

    Testing commit for GitHubBot with broken code.

--- src/resourcemanager.cpp
+++ src/resourcemanager.cpp
@@ -69,8 +69,7 @@ void ResourceManager::setActiveResource(const std::string &resource) {
 }
 
 bool ResourceManager::hasResource(const std::string &r) {
-// 	return m_resources.find(r) != m_resources.end();
-	return false;
+	return m_resources.find(r) != m_resdources.end();
 }
 
 Resource & ResourceManager::getResource(const std::string &r) {
