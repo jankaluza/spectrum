8d22db052cfce4927743829a8473fad0978b23b9
Mathias Ertl <mati@fsinf.at>
2010-05-16 16:10:09 +0200

    more info in setup.py

--- spectrumctl/setup.py
+++ spectrumctl/setup.py
@@ -1,5 +1,11 @@
 from distutils.core import setup
-setup(name='spectrum',
+
+setup(
+	name='spectrum',
 	version='1.0',
+	description='Spectrum control libraries',
+	author='Mathias Ertl',
+	author_email='mati@fsinf.at',
+	url='http://spectrum.im',
 	packages=['spectrum']
 )
