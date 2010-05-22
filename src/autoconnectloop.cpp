7a82b9a26adec3fe2ca34a2fb57b824fbfe5d90b
Jan "HanzZ" Kaluza <hanzz.k@gmail.com>
2010-05-22 15:09:31 +0200

    Send unavailable presences after restart to users who have been logged in before restart.

--- src/autoconnectloop.cpp
+++ src/autoconnectloop.cpp
@@ -34,9 +34,40 @@ static gboolean iter(gpointer data){
 
 AutoConnectLoop::AutoConnectLoop() {
 	m_users = Transport::instance()->sql()->getOnlineUsers();
+
+	// This code blocks and it's alright, because transport should not handle anything before it gets to consistent
+	// state.
+	for (std::vector <std::string>::iterator it = m_users.begin(); it != m_users.end(); it++) {
+		Log("connection restorer", "Sending unavailable presences to " << *it);
+		Tag *stanza = new Tag("presence");
+		stanza->addAttribute( "to", *it);
+		stanza->addAttribute( "type", "unavailable");
+		stanza->addAttribute( "from", Transport::instance()->jid());
+		Transport::instance()->send(stanza);
+		
+		std::list <std::string> roster;
+		UserRow res = Transport::instance()->sql()->getUserByJid(*it);
+		if (res.id != -1) {
+			roster = Transport::instance()->sql()->getBuddies(res.id);
+
+			Tag *tag;
+			for(std::list<std::string>::iterator u = roster.begin(); u != roster.end() ; u++){
+				std::string name = *u;
+				std::for_each( name.begin(), name.end(), replaceBadJidCharacters() );
+
+				tag = new Tag("presence");
+				tag->addAttribute( "to", *it );
+				tag->addAttribute( "type", "unavailable" );
+				tag->addAttribute( "from", name + "@" + Transport::instance()->jid() + "/bot");
+				Transport::instance()->send( tag );
+			}
+			g_usleep(10000);
+		}
+	}
+
 	m_timer = new SpectrumTimer(1000, iter, this);
 	m_timer->start();
-	
+
 	if (m_users.size() == 0)
 		delete this;
 }
