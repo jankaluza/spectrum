8cce7fe68237802849fdb0d8f77afdea4cc25a24
Jan "HanzZ" Kaluza <hanzz.k@gmail.com>
2010-05-22 15:20:35 +0200

    Updated tests.

--- src/tests/autoconnectlooptest.cpp
+++ src/tests/autoconnectlooptest.cpp
@@ -18,16 +18,22 @@ void AutoConnectLoopTest::down (void) {
 void AutoConnectLoopTest::restoreNextConnection() {
 	std::string r1;
 	std::string r2;
+	std::string r3;
+	std::string r4;
 	r1 =	"<presence from='icq.localhost' type='probe' to='user1@example.com'/>";
 	r2 =	"<presence from='icq.localhost' type='probe' to='user2@example.com'/>";
+	r3 =	"<presence from='icq.localhost' type='unavailable' to='user1@example.com'/>";
+	r4 =	"<presence from='icq.localhost' type='unavailable' to='user1@example.com'/>";
 	
 	CPPUNIT_ASSERT (m_loop->restoreNextConnection() == true);
 	CPPUNIT_ASSERT (m_loop->restoreNextConnection() == true);
-	testTagCount(2);
+	testTagCount(4);
 	compare(r1);
 	compare(r2);
+	compare(r3);
+	compare(r4);
 	
 	CPPUNIT_ASSERT (m_loop->restoreNextConnection() == false);
-	testTagCount(2);
+	testTagCount(4);
 	compare(r1);
 }
