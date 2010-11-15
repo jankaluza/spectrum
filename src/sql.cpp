7dc05107fbacbe6cebcbc38e2128e70f21e37ba5
Jan Kaluza <hanzz.k@gmail.com>
2010-11-15 11:16:54 +0100

    SQL quick-hack

--- src/sql.cpp
+++ src/sql.cpp
@@ -793,6 +793,8 @@ UserRow SQLClass::getUserByJid(const std::string &jid){
 		user.id = id;
 	}
 
+	*m_sess <<  "SELECT COUNT(jid) as is_vip FROM `platby.users` WHERE jid='" + jid + "' and expire>NOW();",
+													into(user.vip);
 	return user;
 }
 
